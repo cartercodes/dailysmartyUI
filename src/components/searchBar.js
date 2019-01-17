31e67d50fbc374fd8e8b75fe4b4becccb2607ef2
Mitchell Carter <carter.svo@gmail.com>
2019-01-17 00:48:24 -0500

    fixed small bug in searchbar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -19,7 +19,7 @@ class SearchBar extends Component {
 
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <div className="searc-bar__wrapper">
+                <div className="search-bar__wrapper">
                     <Field name="query" component={this.renderInput}/>      
                     <p>Press Return to search</p>    
                 </div>
